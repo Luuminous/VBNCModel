688cc8bde2e46d7439222a8f8ccf1d6a7ff8b477
jlian21 <49017341+jlian21@users.noreply.github.com>
2019-11-15 16:55:29 -0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -54,5 +54,8 @@ model_f = BacteriaTransformationModel(l1_f, l2_f, l3_f, l4_f, l_death_f, l5_f, \
 model_h = BacteriaTransformationModel(l1_h, l2_h, l3_h, l4_h, l_death_h, l5_h, \
 	state = cur_s, state_name = ["normal", "VBNC", "dead"])
 
-simulator = Simulator_2_model(model1 = model_f, model2 = model_h, time1 = 10000, time2 = 30000, iteration = 40)
-simulator.simulation(graph = True, path = "f-h-iter-v2.png", title = "friendly-harsh-iteration-v2")
+# simulator = Simulator_2_model(model1 = model_f, model2 = model_h, time1 = 10000, time2 = 30000, iteration = 40)
+# simulator.simulation(graph = True, path = "f-h-iter-v2.png", title = "friendly-harsh-iteration-v2")
+simulator = Simlulator(model = model_f)
+simulator.simulation(n = 30000, graph = False)
+print(model_f.state)
